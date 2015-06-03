6d97a1c9570e11d13ad78f84e0edd4a8c9bfebfd
Fred Buchanan <oscillator.b@gmail.com>
2015-06-03 12:04:08 -0400

    Trying to get latlng to show

--- src/JS/loadmap.js
+++ src/JS/loadmap.js
@@ -1,9 +1,35 @@
+var jsonFile = "../JSON/cms.json"
+var map;
+
+function addMarkers()
+{
+    console.log("adding markers...");
+    $(document).ready(function(){
+        console.log("Doc Ready");
+        $.getJSON( jsonFile, function( data ) {
+            console.log("Got json");
+            $.each(data, function(index,value){
+                
+                var myLatlng = new google.maps.LatLng(value.lat,value.lng);
+                console.log(myLatlng);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: 'hello world'
+                });
+                
+            });
+        });
+    });
+}
+
 function initialize() {
         var mapOptions = {
           center: { lat: 0, lng: 0},
           zoom: 2
         };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
+        map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+        addMarkers();
       }
 google.maps.event.addDomListener(window, 'load', initialize);
